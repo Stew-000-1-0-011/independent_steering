56d6d518cf1188bd38146d9b52907607435989de
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2023-02-10 15:51:27 +0900

    indepedent_steering_nodeの初期化を実装

--- src/independent_steering_node.cpp
+++ src/independent_steering_node.cpp
@@ -6,11 +6,12 @@
  * @version 0.1
  * @date 2022-11-17
  * 
+ * @todo RosparamUtilの改善。型と変数名の組を使って、一次オブジェクトを作り、そしてmemcpyとかなら上手くいくと思った。
  */
 
 #include <utility>
 #include <array>
-#include <variant>
+#include <optional>
 
 #include <ros/ros.h>
 
@@ -21,7 +22,8 @@
 #include <independent_steering/Linear2D.h>
 #include <independent_steering/Angular2D.h>
 
-// #include <crs_lib/uninitialized.hpp> // std::variantでよくね？
+// #include <crs_lib/uninitialized.hpp> // std::optionalでよくね？
+#include <crs_lib/rosparam_util.hpp>
 #include <robo_master/md.hpp>
 #include <robo_master/manager.hpp>
 #include <shirasu_md.hpp>
@@ -43,20 +45,60 @@ namespace crs_lib
 					// CRSLib::PidController<float>::Parameters position;
 				} steering;
 
-				u32 drive;
-
 				double gear_ratio_steering_par_motor;
 				double wheel_radius;
 				double distance_from_center;
 				double tangent_direction;
 			};
 
-			std::variant<std::monostate, ros::NodeHandle> nh;
+			std::optional<ros::NodeHandle> nh;
 			double robomasu_pub_tim_duration;
-			std::array<Wheel, 4> wheels;
 			double steering_limit;
-
 			double deadzone;
+			std::array<Wheel, 4> wheels;
+
+			Config(ros::NodeHandle& nh)
+			{
+				using CRSLib::RosparamUtil::get_param;
+				using CRSLib::RosparamUtil::read_param;
+				using CRSLib::RosparamUtil::Param;
+
+				this->nh = nh;
+				auto independent_steering_node_param = get_param(nh, "independent_steering_node");
+				robomasu_pub_tim_duration = read_param<double>(independent_steering_node_param, "pub_tim_duration");
+				steering_limit = read_param<double>(independent_steering_node_param, "steering_limit");
+				deadzone = read_param<double>(independent_steering_node_param, "deadzone");
+
+				auto wheels_param = get_param(independent_steering_node_param, "wheels");
+				for(int i = 0; i < 4; ++i)
+				{
+					auto wheel_param = get_param(wheels_param, i);
+
+					{
+						auto steering_param = get_param(wheel_param, "steering");
+
+						wheels[i].steering.id = read_param<int>(steering_param, "id");
+						
+						constexpr auto init_pid_param = [](CRSLib::PidController<float>::Parameters& pid_param, const Param& pid_rosparam)
+						{
+							pid_param =
+							{
+								static_cast<float>(read_param<double>(pid_rosparam, "p")),
+								static_cast<float>(read_param<double>(pid_rosparam, "i")),
+								static_cast<float>(read_param<double>(pid_rosparam, "d")),
+							};
+						};
+						
+						init_pid_param(wheels[i].steering.current, get_param(steering_param, "current"));
+						init_pid_param(wheels[i].steering.velocity, get_param(steering_param, "velocity"));
+					}
+
+					wheels[i].gear_ratio_steering_par_motor = read_param<double>(wheel_param, "gear_ratio_steering_per_motor");
+					wheels[i].wheel_radius = read_param<double>(wheel_param, "wheel_radius");
+					wheels[i].distance_from_center = read_param<double>(wheel_param, "distance_from_center");
+					wheels[i].tangent_direction = read_param<double>(wheel_param, "tangent_direction");
+				}
+			}
 		};
 
 		struct Inner final
@@ -73,7 +115,7 @@ namespace crs_lib
 			};
 
 			crs_lib::MotorDriver::RoboMaster::RoboMasterMdManager robomasu_manager;
-			std::array<std::variant<std::monostate, SteeringWheel>, 4> wheels{};
+			std::array<std::optional<SteeringWheel>, 4> wheels{};
 			const double deadzone;
 
 			ros::Subscriber linear2d_sub{};
@@ -81,16 +123,16 @@ namespace crs_lib
 			ros::Timer robomasu_pub_tim{};
 
 			Inner(Config& config):
-				robomasu_manager{std::get<1>(config.nh)},
+				robomasu_manager{*config.nh},
 				deadzone{config.deadzone}
 			{
 				for(unsigned int i = 0; i < 4; ++i)
 				{
-					wheels[i].emplace<SteeringWheel>
+					wheels[i].emplace
 					(
 						decltype(SteeringWheel::driver){
-							crs_lib::MotorDriver::ShirasuMd{config.wheels[i].steering.id, std::get<1>(config.nh)},
-							crs_lib::MotorDriver::RoboMaster::Md{std::get<1>(config.nh), robomasu_manager.get_target_current_p(i), config.wheels[i].steering.current, config.wheels[i].steering.velocity, true},
+							crs_lib::MotorDriver::ShirasuMd{config.wheels[i].steering.id, *config.nh},
+							crs_lib::MotorDriver::RoboMaster::Md{*config.nh, robomasu_manager.get_target_current_p(i), config.wheels[i].steering.current, config.wheels[i].steering.velocity, true},
 							config.wheels[i].gear_ratio_steering_par_motor,
 							config.wheels[i].wheel_radius,
 							config.steering_limit
@@ -100,9 +142,9 @@ namespace crs_lib
 					);
 				}
 
-				linear2d_sub = std::get<1>(config.nh).subscribe<independent_steering::Linear2D>("body_linear_velocity", 1, &Inner::body_linear_velocity_callback, this);
-				angular2d_sub = std::get<1>(config.nh).subscribe<independent_steering::Angular2D>("body_angular_velocity", 1, &Inner::body_angular_velocity_callback, this);
-				robomasu_pub_tim = std::get<1>(config.nh).createTimer(ros::Duration{config.robomasu_pub_tim_duration}, &Inner::robomasu_pub_tim_callback, this);
+				linear2d_sub = (*config.nh).subscribe<independent_steering::Linear2D>("body_linear_velocity", 1, &Inner::body_linear_velocity_callback, this);
+				angular2d_sub = (*config.nh).subscribe<independent_steering::Angular2D>("body_angular_velocity", 1, &Inner::body_angular_velocity_callback, this);
+				robomasu_pub_tim = (*config.nh).createTimer(ros::Duration{config.robomasu_pub_tim_duration}, &Inner::robomasu_pub_tim_callback, this);
 			}
 
 			// Crab制御
@@ -113,7 +155,7 @@ namespace crs_lib
 				{
 					for(auto& wheel : wheels)
 					{
-						std::get<1>(wheel).driver.update(NormAng{0.0}, 0.0);
+						(*wheel).driver.update(NormAng{0.0}, 0.0);
 					}
 				}
 				else
@@ -122,7 +164,7 @@ namespace crs_lib
 
 					for(auto& wheel : wheels)
 					{
-						std::get<1>(wheel).driver.update(NormAng{theta}, norm);
+						(*wheel).driver.update(NormAng{theta}, norm);
 					}
 				}
 			}
@@ -132,7 +174,7 @@ namespace crs_lib
 			{
 				for(auto& wheel : wheels)
 				{
-					auto&& wheel_inner = std::get<1>(wheel);
+					auto&& wheel_inner = *wheel;
 					wheel_inner.driver.update(NormAng{wheel_inner.tangent_direction}, body_angular_velocity->z * wheel_inner.distance_from_center);
 				}
 			}
@@ -144,17 +186,13 @@ namespace crs_lib
 		};
 
 		// onInitまで未初期化であるようにするため。
-		std::variant<std::monostate, Inner> inner;
+		std::optional<Inner> inner;
 
 		void onInit() override
 		{
-			auto nh = getNodeHandle();
-			
-			Config config;
-			config.nh = nh;
-			/// TODO: 各コンフィグの読み込み(rosparam_util)
+			Config config{getNodeHandle()};
 
-			inner.emplace<Inner>(config);
+			inner.emplace(config);
 		}
 	};
 }
